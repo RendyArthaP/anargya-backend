b07dfb3eca33da999c391739d4c23a53d831f85f
Rendy Artha <rendyartha312@gmail.com>
2021-06-20 21:42:11 +0700

    fixing register, if email registered

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -8,38 +8,56 @@ module.exports = {
     const { email, password } = req.body
     let users = await UserAnargya.findOne({email})
   
-    if(users && bcrypt.compareSync(password, users.password)) {
-      users = users.toObject()
-      const { password, ...payload } = users
-      const token = jwt.sign(payload, JWT_KEY)
-
-      res.status(200).json({
-        message: "Login Success",
-        data: payload,
-        token
-      })
-    } else {
-      res.status(500).send({
-        message: "Invalid email or password"
-      })
+    try {
+      if(users && bcrypt.compareSync(password, users.password)) {
+        users = users.toObject()
+        const { password, ...payload } = users
+        const token = jwt.sign(payload, JWT_KEY)
+  
+        res.status(200).json({
+          message: "Login Success",
+          data: payload,
+          token
+        })
+      } else {
+        res.status(500).send({
+          message: "Invalid email or password"
+        })
+      }
+    } catch(error) {
+      res.status(500).send(error)
     }
   },
 
   handleRegisterUser: async (req, res) => {
-    const users = req.body
-    const hashPassword = await bcrypt.hash(users.password, 10)
+    const { name, email, password, address, phone} = req.body
 
-    if(!hashPassword) throw new Error('Hash password error')
+    try {
+      const existingUser = await UserAnargya.findOne({ email })
+      if(existingUser) {
+        res.status(400).json({
+          message: "Email Already Registered, Please Register With Another Email"
+        })
+      }
 
-    const User = await UserAnargya.create({
-      ...users,
-      password: hashPassword
-    })
+      const hashPassword = await bcrypt.hash(password, 10)
+      if(!hashPassword) throw new Error('Hash password error')
 
-    res.status(200).json({
-      message: "Register Success",
-      data: User
-    })
+      const User = await UserAnargya.create({
+        name,
+        email,
+        password: hashPassword,
+        address,
+        phone
+      })
+
+      res.status(200).json({
+        message: "Register Success",
+        data: User
+      })
+    } catch(error) {
+      res.status(500).send(error)
+    }
   },
 
   handleUser: async (req,res) => {
